17ba3b91211a1da6f87eed62f838cb3bf7ea36ae
euleryu <66705713+euleryu@users.noreply.github.com>
2024-09-25 19:42:49 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rd.330621.xyz:6443".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
